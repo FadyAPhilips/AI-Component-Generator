6084ac97531b25f2752f2f5504668a70aa5cdfae
Fady Philips <fadyaphilips97@gmail.com>
2025-04-08 15:47:36 -0230

    Fixed unused state variable warning

--- src/App.js
+++ src/App.js
@@ -8,10 +8,8 @@ function App() {
   const [htmlCode, setHtmlCode] = useState("");
 
   const handleGenerate = async (prompt) => {
-    setLoading(true); // Show loading state
     const generatedHTML = await generateHTML(prompt);
     setHtmlCode(generatedHTML);
-    setLoading(false);
   };
 
   return (
